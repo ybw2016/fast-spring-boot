f49f860350e14d945a6860159b7be958164923af
qinweibo <qinweibo-pbj@hfax.com>
2018-11-20 16:02:40 +0800

    init webflux

---
+++ webflux/src/main/java/com/example/webflux/User.java
@@ -0,0 +1,20 @@
+package com.example.webflux;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 输入描述.
+ *
+ * @author : weibo
+ * @version : v1.0
+ * @since : 2018/11/20 14:33
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String name;
+    private int age;
+}
