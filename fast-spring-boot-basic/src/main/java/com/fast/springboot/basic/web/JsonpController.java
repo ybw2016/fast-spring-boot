4f633ef9950909792c03345b5b9cde261a70bd37
bowen.yan <yanbowen-pbj@sinosig.com>
2019-01-05 13:46:34 +0800

    jsonp

---
+++ fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/web/JsonpController.java
@@ -0,0 +1,18 @@
+package com.fast.springboot.basic.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author bowen.yan
+ * @date 2019-01-05
+ */
+@RestController
+public class JsonpController {
+//    @RequestMapping(value = {"/", "/index"})
+//    public String index(HttpServletRequest request) {
+//        return "static/jsonp";
+//    }
+}
