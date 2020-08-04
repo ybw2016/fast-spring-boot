ac5aa8406b14d0b994bc19da4cb68fd525a6797a
bowen.yan <yanbowen-pbj@sinosig.com>
2020-08-04 17:00:44 +0800

    从文本中提取关键信息 优化

--- fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/utils/ExtractTextFromFile.java
+++ fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/utils/ExtractTextFromFile.java
@@ -24,11 +24,12 @@ public class ExtractTextFromFile {
     private static final String RAW_FILE_DIR_BAK = USER_WORK_DIR + "db_export_bak/logText.txt";
 
     public static void main(String[] args) {
+        //System.out.println(extractKeyWord("\"data\":{\"loanResult\":\"99\",\"CODE\":\"999999\","));
         //filterTextByKeyword(RAW_FILE_DIR_BAK);
-        filterTextByKeyword(RAW_FILE_DIR_BAK, "补偿地址 url = ", "");
+        filterTextByKeyword(RAW_FILE_DIR_BAK, "KEYWORD，", "，rsp={\"code\":1000", true);
     }
 
-    private static void filterTextByKeyword(String filePath, String prefix, String suffix) {
+    private static void filterTextByKeyword(String filePath, String prefix, String suffix, boolean searchResult) {
         File file = new File(filePath);
         List<String> textList = new ArrayList<>();
         String rawStrLine;
@@ -58,6 +59,11 @@ public class ExtractTextFromFile {
 
                     strLine = strLine.substring(startIndex + skipPrefix, endIndex);
                     System.out.println(String.format("--------> prefix:%s, suffix:%s, strLine:%s", prefix, suffix, strLine));
+
+                    if (searchResult) {
+                        strLine = String.format("%s ---> %s", strLine, extractKeyWord(rawStrLine));
+                        System.out.println(String.format("--------> strLine with keyWord:%s", strLine));
+                    }
                 }
                 if (textList.contains(strLine)) {
                     System.out.println("筛选后的文本已存在 -> " + strLine);
@@ -76,30 +82,15 @@ public class ExtractTextFromFile {
         textList.forEach(System.out::println);
     }
 
-    private static void filterTextByKeyword(String filePath) {
-        File file = new File(filePath);
-        List<String> textList = new ArrayList<>();
-        String strLine;
-
-        try (FileInputStream fileInputStream = new FileInputStream(file);
-             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
-            while ((strLine = bufferedReader.readLine()) != null) {
-                if (textList.contains(strLine)) {
-                    System.out.println("文本已存在 -> " + strLine);
-                    continue;
-                }
-                textList.add(strLine);
+    private static String extractKeyWord(String inputText) {
+        try {
+            String keyWord = "\"loanResult\"";
+            if (inputText.contains(keyWord)) {
+                int startIndex = inputText.indexOf(keyWord);
+                return inputText.substring(startIndex, startIndex + keyWord.length() + 5);
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
         }
-
-        System.out.println();
-        System.out.println();
-        textList.forEach(System.out::println);
-
+        return "";
     }
 }
