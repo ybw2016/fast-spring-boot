540aa3f5652e0a0341a34508cdddc39bdbb26bdd
bowen.yan <yanbowen-pbj@sinosig.com>
2020-11-16 10:01:55 +0800

    添加布隆过滤器验证类

---
+++ fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/algorithm/BloomFilterTest.java
@@ -0,0 +1,32 @@
+package com.fast.springboot.basic.algorithm;
+
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author bw
+ * @since 2020-11-16
+ */
+public class BloomFilterTest {
+    private static int size = 1000000;
+
+    private static BloomFilter<Integer> bloomFilter = BloomFilter.create(Funnels.integerFunnel(), size);
+
+    public static void main(String[] args) {
+        for (int i = 0; i < size; i++) {
+            bloomFilter.put(i);
+        }
+
+        List<Integer> list = new ArrayList<Integer>(1000);
+        //故意取10000个不在过滤器里的值，看看有多少个会被认为在过滤器里
+        for (int i = size + 10000; i < size + 20000; i++) {
+            if (bloomFilter.mightContain(i)) {
+                list.add(i);
+            }
+        }
+        System.out.println("误判的数量：" + list.size());
+    }
+}
