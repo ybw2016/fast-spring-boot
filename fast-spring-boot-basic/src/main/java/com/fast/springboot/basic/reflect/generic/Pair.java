f8a32fd5e443d07792ff355eb68d44758fed9695
bowen.yan <yanbowen-pbj@sinosig.com>
2020-11-29 22:21:41 +0800

    泛型

--- fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/reflect/generic/Pair.java
+++ fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/reflect/generic/Pair.java
@@ -14,4 +14,30 @@ public class Pair<T> {
     public void setValue(T value) {
         this.value = value;
     }
+
+    /*  编译后泛型擦除:
+
+    bw$ javap -c Pair.class
+    Compiled from "Pair.java"
+    public class com.fast.springboot.basic.reflect.generic.Pair<T> {
+  public com.fast.springboot.basic.reflect.generic.Pair();
+        Code:
+        0: aload_0
+        1: invokespecial #1                  // Method java/lang/Object."<init>":()V
+        4: return
+
+        public T getValue();
+        Code:
+        0: aload_0
+        1: getfield      #2                  // Field value:Ljava/lang/Object;
+        4: areturn
+
+        public void setValue(T);
+        Code:
+        0: aload_0
+        1: aload_1
+        2: putfield      #2                  // Field value:Ljava/lang/Object;
+        5: return
+    }
+*/
 }
--- fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/reflect/superextends/SuperExtendsTest.java
+++ fast-spring-boot-basic/src/main/java/com/fast/springboot/basic/reflect/superextends/SuperExtendsTest.java
@@ -1,6 +1,8 @@
 package com.fast.springboot.basic.reflect.superextends;
 
 /**
+ * 参考链接： https://www.cnblogs.com/JasonLGJnote/p/11159869.html
+ *
  * @author bw
  * @since 2020-11-27
  */
@@ -38,7 +40,15 @@ public class SuperExtendsTest {
 
     public static void main(String[] args) {
         /*
+        //
+        ====================> <? extends T>：是指 “上界通配符（Upper Bounds Wildcards）” <====================
+        原因是编译器只知道容器内是Fruit或者它的派生类，但具体是什么类型不知道。可能是Fruit？可能是Apple？
+        也可能是Banana，RedApple，GreenApple？编译器在看到后面用Plate赋值以后，盘子里没有被标上有“苹果”。
+        [而是标上一个占位符：CAP#1，来表示捕获一个Fruit或Fruit的子类，具体是什么类不知道，代号CAP#1]。
+        然后无论是想往里插入Apple或者Meat或者Fruit编译器都不知道能不能和这个CAP#1匹配，所以就都不允许。
         Plate<? extends Fruit> plateFruit = new Plate<Apple>(new Apple());
+
+        ------>   类型占位符 CAP#1  !=   具体的子类型（Apple、RedApple等）
 
         //不能存入任何元素
         plateFruit.set(new Fruit());    //Error
@@ -48,6 +58,26 @@ public class SuperExtendsTest {
         Fruit newFruit1 = plateFruit.get();
         Object newFruit2 = plateFruit.get();
         Apple newFruit3 = plateFruit.get();    //Error
+
+
+
+
+        ====================> <? super T>：是指 “下界通配符（Lower Bounds Wildcards）” <====================
+        下界<? super T>不影响往里存，但往外取只能放在Object对象里
+        使用下界<? super Fruit>会使从盘子里取东西的get( )方法部分失效，只能存放到Object对象里。set( )方法正常。
+        Plate<? super Fruit> pfruit=new Plate<Fruit>(new Fruit());
+
+        //存入元素正常
+        pfruit.set(new Fruit());
+        pfruit.set(new Apple());
+
+        //读取出来的东西只能存放在Object类里。
+        Apple newFruit3=pfruit.get();    //Error
+        Fruit newFruit1=pfruit.get();    //Error
+        Object newFruit2=pfruit.get();
+
+        因为下界规定了元素的最小粒度的下限，实际上是[放松了容器元素的类型控制]。既然元素是Fruit的基类，
+        那往里存粒度比Fruit小的都可以。但往外读取元素就费劲了，只有所有类的基类Object对象才能装下。但这样的话，元素的类型信息就全部丢失。
         */
     }
 }
